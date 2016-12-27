0e109481afe2e00f1d792c18ecc6db6ba32bc769
Joeun <imjoeunha@gmail.com>
2016-12-27 23:36:12 +0900

    Create index.html / $ variable, switch statement

--- cQuery.js
+++ cQuery.js
@@ -1,2 +1,22 @@
 /* 1. Selector */
+
+var $ = function(str) {
+  var selector = str.substr(0, 1);
+
+  switch (selector) {
+    case '#':
+      console.log("Selector is Id");
+      break;
+    case '.':
+      console.log("Selector is ClassName");
+      break;
+    default:
+      console.log("Selector is TagName");
+  }
+}
+
+$('div') // "Selector is TagName"
+$('.container') // "Selector is ClassName"
+$('#element01') // "Selector is Id"
+
 /* 2. Event Listener */
